7862f3830cb2c9207525d72c8e8c05caa8d3d170
Vlad Schnakovszki <shnako@yahoo.com>
2019-05-09 08:59:31 +0100

    Added logging, exception handling and account number parameter validation to the MeterReadsResource.

--- tech-test/src/main/java/uk/co/scottishpower/techtest/TechTestApplication.java
+++ tech-test/src/main/java/uk/co/scottishpower/techtest/TechTestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechTestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TechTestApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TechTestApplication.class, args);
+    }
 }
--- tech-test/src/main/java/uk/co/scottishpower/techtest/rest/smart/MeterReadsResource.java
+++ tech-test/src/main/java/uk/co/scottishpower/techtest/rest/smart/MeterReadsResource.java
@@ -1,5 +1,7 @@
 package uk.co.scottishpower.techtest.rest.smart;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import uk.co.scottishpower.techtest.repository.MeterReadRepository;
 
 @RestController
 @RequestMapping("/api/smart/reads")
+@Slf4j
 public class MeterReadsResource {
     private final MeterReadRepository meterReadRepository;
 
@@ -19,6 +22,17 @@ public class MeterReadsResource {
 
     @GetMapping("/{" + Params.ACCOUNT_NUMBER + "}")
     public ResponseEntity getReads(@PathVariable(Params.ACCOUNT_NUMBER) long accountNumber) {
-        return ResponseEntity.ok(meterReadRepository.getByAccountNumber(accountNumber));
+        if (accountNumber <= 0) {
+            log.warn("Query rejected for invalid account number {}", accountNumber);
+            return ResponseEntity.badRequest().body("Account number must be positive");
+        }
+
+        try {
+            log.info("Loading smart meter details for account number {}", accountNumber);
+            return ResponseEntity.ok(meterReadRepository.getByAccountNumber(accountNumber));
+        } catch (Exception ex) {
+            log.error("Unexpected error while loading meter reading", ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
