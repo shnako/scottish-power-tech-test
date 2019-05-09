55b37ca42c86d015acc595cdc7076aeb502877d2
Vlad Schnakovszki <shnako@yahoo.com>
2019-05-09 09:09:37 +0100

    Removed unwanted fields from response

--- tech-test/src/main/java/uk/co/scottishpower/techtest/model/MeterRead.java
+++ tech-test/src/main/java/uk/co/scottishpower/techtest/model/MeterRead.java
@@ -1,5 +1,6 @@
 package uk.co.scottishpower.techtest.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -13,15 +14,18 @@ import javax.validation.constraints.Positive;
 @Entity
 public class MeterRead {
     @Id
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(message = "The account number must be specified")
     @Positive(message = "The account number must be positive")
     private Long accountNumber;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(message = "The gas id must be specified")
     @Min(value = 100000L, message = "The gas id must contain at least 6 digits")
     @Max(value = 9999999999L, message = "The gas id must contain at most 10 digits")
     private Long gasId;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(message = "The electricity id must be specified")
     @Min(value = 1000000000000L, message = "The electricity id must contain 13 digits")
     @Max(value = 9999999999999L, message = "The electricity id must contain 13 digits")
